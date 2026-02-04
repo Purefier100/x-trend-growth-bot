fc5e7559b8a5433e2d36edc3a64b582c8407666e
Purefier100 <benchristel5@gmail.com>
2026-02-04 22:24:28 +0100

    Add missing targets file

---
+++ targets.js
@@ -0,0 +1,108 @@
+// targets.js
+export const TARGET_ACCOUNTS = [
+    // infra / airdrops
+    "BuildOnBase",
+    "coinbase",
+    "LayerZero_Labs",
+    "wormholecrypto",
+    "Scroll_ZKP",
+    "Starknet",
+    "arbitrum",
+    "optimismPBC",
+
+    // airdrop hunters
+    "0xngmi",
+    "CC2Ventures",
+    "DeFi_Mochi",
+    "CryptoLeek",
+    "Airdrop_Detect",
+
+    // base / meme scouts
+    "BasedMigs",
+    "BaseGod",
+    "basedkarbon",
+    "onchainwizard",
+    "base",
+    "optimism",
+    "coinbase",
+    "paradigm",
+    "0xngmi",
+    "defi_monk",
+    "chainyoda",
+    "punk6529",
+    "cristiano",
+    "elomusk",
+    "vitalikbuterin",
+    "instablog9ja",
+    "param_eth",
+    "waleswoosh",
+    "bulktrade",
+    "CryptoCobain",
+    "0xTodd",
+    "jackbutcher",
+    "6529Museum",
+    "punk6529",
+    "Beaniemaxi",
+
+    // Degen signalers
+    "DegenSpartan",
+    "pentosh1",
+    "HsakaTrades",
+    "route2fi",
+    "CL207",
+
+    // Meme accelerators
+    "FrankDeGods",
+    "pranksy",
+    "icebergy_",
+    "farokh",
+    "Zeneca",
+
+    // Base / ETH meme-adjacent
+    "jessepollak",
+    "base",
+    "coinbase",
+
+    // Pseudonymous alpha meme CT
+    "rektfencer",
+    "0xfoobar",
+    "TraderSz",
+    "CryptoKaleo",
+    "MachiBigBrother",
+    // Core infra
+    "base",
+    "optimism",
+    "coinbase",
+    "ethereum",
+    "zksync",
+
+    // Builders / devs
+    "0xSacha",
+    "danromero",
+    "jessepollak",
+    "vbuterin",
+    "bantg",
+    "transmissions11",
+
+    // Funds / research
+    "paradigm",
+    "a16zcrypto",
+    "dragonfly_xyz",
+    "variantfund",
+    "hasufl",
+
+    // Onchain thinkers
+    "0xngmi",
+    "punk6529",
+    "route2fi",
+    "defi_monk",
+    "chainyoda",
+
+    // Traders / early hunters
+    "HsakaTrades",
+    "pentosh1",
+    "DegenSpartan",
+    "CryptoCobain",
+    "rektfencer",
+    "cl207"
+];
